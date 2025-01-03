255529ba8a8264fda29060ace0b564fd8120093d
ko-tarou <tkota6174@gmail.com>
2025-01-03 17:08:28 +0900

    icon追加

--- src/pages/HeaderPage/index.js
+++ src/pages/HeaderPage/index.js
@@ -1,7 +1,10 @@
 import styles from "../../styles/HeaderPage/Header.module.scss";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function HeaderPage() {
 return (
-	<div className={styles.header}></div>
+	<div className={styles.header}>
+        <MenuIcon/>
+    </div>
 );
 }
