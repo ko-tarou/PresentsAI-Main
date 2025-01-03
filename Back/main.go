35022d41515aeeed03ea22af9573bd9d4572ca86
ko-tarou <tkota6174@gmail.com>
2025-01-03 15:14:15 +0900

    ファイル分割

---
+++ Back/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func initDB() {
+	dsn := "host=localhost user=postgres password=pokota dbname=PresentsAI port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	db.AutoMigrate(&User{},&UserToProject{})
+	fmt.Println("Database connected and User table migrated")
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*") // 全てのオリジンを許可
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == "OPTIONS" { // プリフライトリクエストへの対応
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+
+	initDB()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/users", createUser).Methods("POST")
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+
+	fmt.Println("Server is running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", enableCORS(router)))
+}
--- Back/user.go
+++ Back/user.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
 	"time"
+	"fmt"
 
 	"github.com/gorilla/mux"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 type User struct {
@@ -28,19 +25,11 @@ type UserToProject struct {
 	ProjectID uint
 }
 
-var db *gorm.DB
 
-func initDB() {
-	dsn := "host=localhost user=postgres password=pokota dbname=PresentsAI port=5432 sslmode=disable TimeZone=Asia/Tokyo"
-	var err error
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
 
-	db.AutoMigrate(&User{},&UserToProject{})
-	fmt.Println("Database connected and User table migrated")
-}
+
+
+
 
 func createUser(w http.ResponseWriter, r *http.Request) {
 	var user User
@@ -83,31 +72,4 @@ func deleteUser(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "User with ID %s deleted", id)
-}
-
-
-func enableCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*") // 全てのオリジンを許可
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		if r.Method == "OPTIONS" { // プリフライトリクエストへの対応
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func main() {
-
-	initDB()
-
-	router := mux.NewRouter()
-	router.HandleFunc("/users", createUser).Methods("POST")
-	router.HandleFunc("/users", getUsers).Methods("GET")
-	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
-
-	fmt.Println("Server is running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", enableCORS(router)))
 }
