5f0be39f0acb2f962e270eb90e209f3d45ebad77
ko-tarou <tkota6174@gmail.com>
2025-01-03 17:25:44 +0900

    IconをButtonにできた

--- src/pages/HeaderPage/index.js
+++ src/pages/HeaderPage/index.js
@@ -1,10 +1,22 @@
 import styles from "../../styles/HeaderPage/Header.module.scss";
 import MenuIcon from "@mui/icons-material/Menu";
 
+//Icon
+import IconButton from "@mui/material/IconButton";
+
 export default function HeaderPage() {
-return (
-	<div className={styles.header}>
-        <MenuIcon/>
-    </div>
-);
+
+    const handleClick = () => {
+        console.log("Button clicked");
+    };
+
+    return (
+        <div className={styles.header}>
+            <IconButton
+                className={styles.button} 
+                onClick={handleClick}>
+                <MenuIcon />
+            </IconButton>
+        </div>
+    );
 }
