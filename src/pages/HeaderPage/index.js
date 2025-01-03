e74d021aec9f35715e32767bb0cb7f38423e88ee
ko-tarou <tkota6174@gmail.com>
2025-01-03 16:42:41 +0900

    headerが出来た。あとボタン付けるだけ

--- src/pages/HeaderPage/index.js
+++ src/pages/HeaderPage/index.js
@@ -2,9 +2,6 @@ import styles from "../../styles/HeaderPage/Header.module.scss";
 
 export default function HeaderPage() {
 return (
-	<div className={styles.header}>
-        <h1>headerページ</h1>
-        <p>ここはheaderページです。</p>
-	</div>
+	<div className={styles.header}></div>
 );
 }
