5c3e59760a988691142fb21f8c4ed392478d3719
ko-tarou <tkota6174@gmail.com>
2025-01-03 18:07:29 +0900

    色の追加とドロップダウンメニュー追加

--- src/pages/HeaderPage/index.js
+++ src/pages/HeaderPage/index.js
@@ -1,22 +1,59 @@
+import { useState } from "react";
 import styles from "../../styles/HeaderPage/Header.module.scss";
 import MenuIcon from "@mui/icons-material/Menu";
 
 //Icon
 import IconButton from "@mui/material/IconButton";
 
-export default function HeaderPage() {
+// Material-UI components
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
-    const handleClick = () => {
-        console.log("Button clicked");
+export default function HeaderPage() {
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
     };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
 
     return (
         <div className={styles.header}>
-            <IconButton
-                className={styles.button} 
-                onClick={handleClick}>
-                <MenuIcon />
-            </IconButton>
+            <div className={styles.content}>
+
+
+                {/* メニューボタン */}
+                <IconButton
+                    className={styles.button} 
+                    onClick={handleClick}>
+                    <MenuIcon />
+                </IconButton>
+
+				{/* ドロップダウンメニュー */}
+                <Menu
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "right",
+                    }}
+                    transformOrigin={{
+                        vertical: "top",
+                        horizontal: "right",
+                    }}
+                    classes={{
+                        paper: styles.menu,
+                    }}
+                    >
+                    <MenuItem onClick={handleClose}>メニュー項目1</MenuItem>
+                    <MenuItem onClick={handleClose}>メニュー項目2</MenuItem>
+                    <MenuItem onClick={handleClose}>メニュー項目3</MenuItem>
+                </Menu>
+            </div>
         </div>
     );
 }
