4d2342f079d7f475a06d659ccaa274cf80a7c4e1
ko-tarou <tkota6174@gmail.com>
2025-01-03 16:32:18 +0900

    scss導入

--- src/pages/HeaderPage/index.js
+++ src/pages/HeaderPage/index.js
@@ -1,8 +1,10 @@
+import styles from "../../styles/HeaderPage/Header.module.css";
+
 export default function HeaderPage() {
-	return (
-	<div>
-		<h1>headerページ</h1>
-		<p>ここはheaderページです。</p>
+return (
+	<div className={styles.header}>
+        <h1>headerページ</h1>
+        <p>ここはheaderページです。</p>
 	</div>
-	);
+);
 }
---
+++ src/pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/Global/global.css';
+import '../styles/Global/color.css';
+
+export default function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />;
+}
