429337aa762f3da1f7cafea6ccb90de546a46e13
Riku Shioda <hry-23@Rikus-MacBook-Pro.local>
2025-01-25 14:23:11 +0900

    uiの変更

--- src/pages/TopPage/index.js
+++ src/pages/TopPage/index.js
@@ -1,32 +1,34 @@
 import { useState } from 'react';
-import HeaderPage from "../HeaderPage/index.js"
+import HeaderPage from "../HeaderPage/index.js";
 import styles from '../../styles/TopPage/TopPage.module.scss';
-import {Home,Settings} from "@mui/icons-material"
+import { Home, Settings } from "@mui/icons-material";
 
 export default function TopPage() {
   const [activeTab, setActiveTab] = useState('tab1');
 
   return (
     <div>
-      <HeaderPage/>
-      <div style={{ display: 'flex', gap: '10px', marginBottom: '20px' }}>
+      <HeaderPage />
+      <div className={styles.Box}>
+        {/* ボタンを縦並びに配置 */}
         <button
           onClick={() => setActiveTab('tab1')}
           className={`${styles.button} ${activeTab !== 'tab1' ? styles.inactive : ''}`}
         >
-          <Home/>
+          <Home className={styles.Home} />
         </button>
         <button
           onClick={() => setActiveTab('tab2')}
           className={`${styles.button} ${activeTab !== 'tab2' ? styles.inactive : ''}`}
         >
-          <Settings/>
+          <Settings className={styles.Settings} />
         </button>
-      </div>
 
-      <div>
-        {activeTab === 'tab1' && <div>タブ 1 の内容がここに表示されます。</div>}
-        {activeTab === 'tab2' && <div>タブ 2 の内容がここに表示されます。</div>}
+        {/* コンテンツ */}
+        <div>
+          {activeTab === 'tab1' && <div>タブ 1 の内容がここに表示されます。</div>}
+          {activeTab === 'tab2' && <div>タブ 2 の内容がここに表示されます。</div>}
+        </div>
       </div>
     </div>
   );
