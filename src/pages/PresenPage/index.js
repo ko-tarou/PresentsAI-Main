8c405064cf02815a5c63e11421e87e150f97488e
karir <c1406274@st.kanazawa-it.ac.jp>
2025-01-25 18:52:03 +0900

    一時補zん

--- src/pages/PresenPage/index.js
+++ src/pages/PresenPage/index.js
@@ -1,10 +1,17 @@
+import styles from '../../styles/PresenPage/PresenPage.module.scss'
+
 export default function PresenPage() {
 	return (
-    <div>
-		<h1>プレゼンページ</h1>
-        <p>ここはプレゼンページです。</p>
+	<div className={styles.pageBackground}>
+		<div className={styles.PresenPageSidebar}>か</div>
 		<div className={styles.aspectRatioBox}>
+			<div className={styles.Timer}>00 : 00</div>
 			<div className={styles.content}>16:9 四角形</div>
 		</div>
-    </div>);
+		<div className={styles.memoaiassist}>
+			<div className={styles.memo}>具体的に言うと…</div>
+			<div className={styles.aiassist}>私たちはPresentsAIについて説明します</div>
+		</div>
+    </div>
+	);
 }
